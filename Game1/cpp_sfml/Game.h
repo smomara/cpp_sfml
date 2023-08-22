a40d0da8f82d805110de817e7a1b878ada26dd8b
Sean O'Mara <omarams@cua.edu>
2023-08-22 19:52:46 -0400

    Update Game.h

--- Game1/cpp_sfml/Game.h
+++ Game1/cpp_sfml/Game.h
@@ -1,6 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <sstream>
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -22,10 +27,39 @@ private:
 	sf::VideoMode videoMode;
 	sf::Event ev;
 
+	// Mouse positions
+	sf::Vector2i mousePosWindow;
+	sf::Vector2f mousePosView;
+
+	// Game logic
+	bool endgame;
+	unsigned points;
+	int health;
+	float enemySpawnTimer;
+	float enemySpawnTimerMax;
+	int maxEnemies;
+	bool mouseHeld;
+
+	// Resources
+	sf::Font font;
+
+	// Text
+	sf::Text uiText;
+
+	// Game objects
+	std::vector<sf::RectangleShape> enemies;
+	sf::RectangleShape enemy;
+	
+	sf::RectangleShape healthbarOutline;
+	sf::RectangleShape healthbarHealth;
+
 	// Private functions
 	void initVariables();
 	void initWindow();
-
+	void initEnemies();
+	void initFonts();
+	void initText();
+	void initHealthbar();
 
 public:
 	// Constructors + Destructors
@@ -34,12 +68,22 @@ public:
 
 	// Accessors
 	const bool running() const;
+	const bool getEndgame() const;
 
 	// Functions
+	void spawnEnemy();
+
 	void pollEvents();
+	void updateMousePositions();
+	void updateText();
+	void updateHealthbar();
+	void updateEnemies();
 	void update();
+
+	void renderText(sf::RenderTarget& target);
+	void renderHealthbar(sf::RenderTarget& target);
+	void renderEnemies(sf::RenderTarget& target);
 	void render();
 };
 
 #endif
-
